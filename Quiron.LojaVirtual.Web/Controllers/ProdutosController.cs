ad8bcbde39d141fc1b50584fd0a5f7fcf4bf0d99
Bernardo <bernardoholtz@gmail.com>
2014-09-06 14:03:15 -0300

    Paginacao

---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        private ProdutoRepositorio _repositorio;
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutoRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+
+            return View(produtos);
+
+        }
+    }
+}
